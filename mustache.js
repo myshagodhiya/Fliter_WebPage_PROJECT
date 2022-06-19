7de0cbab7c1ffaf72677f842d699e8c467ad922c
myshagodhiya <80570084+myshagodhiya@users.noreply.github.com>
2022-06-19 18:17:48 +0530

    Add files via upload

---
+++ mustache.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+    mustache_filter= loadImage('https://i.postimg.cc/VkkqXxKZ/TRANSPARENT-MUSTACHE-FINALL.png');
+}
+
+function setup(){
+        canvas=createCanvas(280,280);
+        canvas.center();
+        background("white");
+    canvas=createCanvas(330,330);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(330,330);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0);
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
+function draw(){
+    image(video,0,0,330,330);
+    image(mustache_filter, noseX-20, noseY-6, 40, 40);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
---
+++ welcome.js
@@ -0,0 +1,9 @@
+function begin(){
+    window.location="mustache.html";
+}
+
+function about(){
+    window.alert("THE MODAL OF THE ABOUT BUTTON WILL BE ACTIVATED AFTER LEARNING ABOUT THE MODAL.");
+}
+
+
